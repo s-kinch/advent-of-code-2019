ba6eab52e265eacfe793d2d6eeb6e90fe3044797
Sinclair Kinch <sinclair.kinch@us.sogeti.com>
2019-12-10 12:45:33 -0500

    day 4: part 2

--- day4.js
+++ day4.js
@@ -1,5 +1,6 @@
 const splitToDigitsArray = num => `${num}`.split('').map(digit => parseInt(digit))
 
+// Part 1
 const isIncreasingAndContainsADouble = arr => {
     let doubleFound = false
     for (let i = 1; i < arr.length; i++){
@@ -9,7 +10,27 @@ const isIncreasingAndContainsADouble = arr => {
     return doubleFound
 }
 
+// Part 2
+const isIncreasingAndContainsADoubleThatIsNotPartOfALargerGroup = arr => {
+    let doubleFound = false
+    let numberInGroup = 1
+    let currentNum = arr[0]
+    for (let i = 1; i < arr.length; i++){
+        if (arr[i] < currentNum) return false
+        if (arr[i] === currentNum) {
+            numberInGroup++
+            if (i === arr.length - 1 && numberInGroup === 2) doubleFound = true
+        } else {
+            if (numberInGroup === 2) doubleFound = true
+            currentNum = arr[i]
+            numberInGroup = 1
+        }
+    }
+    return doubleFound
+}
+
+
 const intsInRange = (low, high) => [...Array(high - low + 1).keys()].map(x => x + low)
 
-const matches = intsInRange(168630, 718098).map(int => splitToDigitsArray(int)).filter(int => isIncreasingAndContainsADouble(int))
+const matches = intsInRange(168630, 718098).map(int => splitToDigitsArray(int)).filter(int => isIncreasingAndContainsADoubleThatIsNotPartOfALargerGroup(int))
 console.log(matches.length)
